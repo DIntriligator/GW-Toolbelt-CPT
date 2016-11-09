499c4338273fc3cd792cbb499b999cb1ecd1dc75
DIntriligator <DIntriligator@gmail.com>
2016-11-09 17:42:25 -0500

    sub-menu added

--- GW-Toolbelt-CPT.php
+++ GW-Toolbelt-CPT.php
@@ -7,3 +7,38 @@ Description: A Group of plugins aimed at making the development of wordpress plu
 Author: Graphics Westchester
 Version: 0.0.00
 */
+
+/*
+*  gwtb_admin_status_menu
+*
+*  This function adds a Custom Post Types UI section to view the status of addon settings.
+*
+*  @type    function
+*  @date    11/09/16
+*  @since   0.0.00
+*
+*  @param   N/A
+*  @return  N/A
+*/
+
+function gwtb_admin_cpt_menu(){
+	$menu = add_submenu_page( 'gwtoolbelt', 'Custom Post Types', 'Custom Post Types', 'manage_options', 'gwtb-cpt', 'gwtb_cpt_init' );
+}
+add_action( 'admin_menu', 'gwtb_admin_cpt_menu', 3 );
+
+/*
+*  gwtb_admin_status_menu
+*
+*  This function adds a page layout for the gwtb_admin_cpt_menu function
+*
+*  @type    function
+*  @date    11/09/16
+*  @since   0.0.00
+*
+*  @param   N/A
+*  @return  N/A
+*/
+
+function gwtb_cpt_init(){
+	include(dirname(__FILE__) . '/admin-cpt.php');
+}
---
+++ admin-cpt.php
@@ -0,0 +1,4 @@
+<?php 
+$title = "CUSTOM POST TYPES"; 
+include(GWTB_PLUGIN_DIR . 'layout/header.php') 
+?>
