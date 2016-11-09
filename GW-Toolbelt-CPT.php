04a034ace1e3a6863570f5b8b045c2fffa2e025f
DIntriligator <DIntriligator@gmail.com>
2016-11-09 17:05:15 -0500

    init commit

---
+++ GW-Toolbelt-CPT.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: GW Toolbelt CPT
+GitHub Plugin URI: DIntriligator/GW-Toolbelt-CPT
+GitHub Plugin URI: https://github.com/DIntriligator/GW-Toolbelt-CPT
+Description: A Group of plugins aimed at making the development of wordpress plugins easier. This main plugin will serve as a hub for all of the others
+Author: Graphics Westchester
+Version: 0.0.00
+*/
